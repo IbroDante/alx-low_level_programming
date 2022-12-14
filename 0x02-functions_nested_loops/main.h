f82f128bf3ac7f319d081df678798ae40888fb4b
Ibrodante <ibroman02@yahoo.com>
2022-12-14 19:55:51 +0000

    another commit on loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,6 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
-/*
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -15,6 +14,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-*/
 
 #endif
