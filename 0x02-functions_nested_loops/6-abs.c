4fbcd6364a0a292819b0dccc2e25855062a76890
Ibrodante <ibroman02@yahoo.com>
2022-12-14 21:03:53 +0000

    another commit on loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - compute the abs value of an int
+ * @t: the int to be compute
+ * Return: the abs val
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
